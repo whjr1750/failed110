319f8de974e405385b4c71d2fa4faf4df6835de9
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2023-05-24 12:14:52 +0530

    Updated detect_face_with_mask.py

--- detect_face_with_mask.py
+++ detect_face_with_mask.py
@@ -12,7 +12,13 @@ while(True):
       
     # Capture the video frame by frame
     ret, frame = vid.read()
-    prediction = model.predict(frame)
+    img = cv2.resize(frame,(224,224))
+    test_image = np.array(img, dtype=np.float32)
+    test_image = np.expand_dims(test_image, axis=0)
+
+    # 3. Normalizing the image
+    normalised_image = test_image/255.0
+    prediction = model.predict(normalised_image)
     print("prediction",prediction)
     # Display the resulting frame
     cv2.imshow('frame', frame)
